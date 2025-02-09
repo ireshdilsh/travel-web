1a6fbd76a2d267344207ebea5be13733a0d9e10f
ireshdilsh <ireshdilshan46@gmail.com>
2025-02-09 22:28:52 +0530

    update Bootstrap version in index.html and add navigation structure to Landing component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,7 +3,14 @@ import "../styles/Landing.css"
 export default function Landing() {
   return (
     <div>
-     ksldfk lksdf
+        <nav>
+            <div className="logo-name">
+                <h4>Literary <span>Haven</span></h4>
+            </div>
+            <div className="links">
+                
+            </div>
+        </nav>
     </div>
   )
 }
