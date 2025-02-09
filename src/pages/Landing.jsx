30ac49a30f966c1e5762ede6b31d4e14e9ce96c1
ireshdilsh <ireshdilshan46@gmail.com>
2025-02-09 22:44:33 +0530

    add navigation links and enhance styling for Landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -8,7 +8,14 @@ export default function Landing() {
                 <h4>Literary <span>Haven</span></h4>
             </div>
             <div className="links">
-                
+                <a href="">Home</a>
+                <a href="">Books</a>
+                <a href="">Offers</a>
+                <a href="">Service</a>
+                <a href="">Get in Touch</a>
+            </div>
+            <div className="signup-btn">
+                <button className='btn btn-primary'>Sign Up</button>
             </div>
         </nav>
     </div>
