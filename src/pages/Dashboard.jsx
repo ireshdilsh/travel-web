c1c095aaf24d4ff277d9df61ef3dadf24567bd19
ireshdilsh <ireshdilshan46@gmail.com>
2025-02-12 17:45:09 +0530

    add boostrap according

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "../styles/Dashboard.css"
 
 export default function Dashboard() {
+
     return (
         <div>Dashboard</div>
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -22,8 +22,6 @@ export default function Landing() {
             <div className="signin-btn">
                 <Button>Sign In</Button>
             </div>
-
-
         </nav>
 
         <section id="home">
@@ -182,11 +180,98 @@ export default function Landing() {
         <section id="service">
             <h4>Our Services</h4>
             <p>We offer a wide range of services, including personalized book recommendations, gift wrapping, and fast shipping to enhance your shopping experience.</p>
+
+            <div className="accordion accordion-flush" id="accordionFlushExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingOne">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#flush-collapseOne" aria-expanded="false"
+                                aria-controls="flush-collapseOne">
+                            Accordion Item #1
+                        </button>
+                    </h2>
+                    <div id="flush-collapseOne" className="accordion-collapse collapse"
+                         aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">Placeholder content for this accordion, which is intended to
+                            demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion
+                            body.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingTwo">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#flush-collapseTwo" aria-expanded="false"
+                                aria-controls="flush-collapseTwo">
+                            Accordion Item #2
+                        </button>
+                    </h2>
+                    <div id="flush-collapseTwo" className="accordion-collapse collapse"
+                         aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">Placeholder content for this accordion, which is intended to
+                            demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion
+                            body. Let's imagine this being filled with some actual content.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingThree">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#flush-collapseThree" aria-expanded="false"
+                                aria-controls="flush-collapseThree">
+                            Accordion Item #3
+                        </button>
+                    </h2>
+                    <div id="flush-collapseThree" className="accordion-collapse collapse"
+                         aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">Placeholder content for this accordion, which is intended to
+                            demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion
+                            body. Nothing more exciting happening here in terms of content, but just filling up the
+                            space to make it look, at least at first glance, a bit more representative of how this would
+                            look in a real-world application.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingFour">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#flush-collapseFour" aria-expanded="false"
+                                aria-controls="flush-collapseFour">
+                            Accordion Item #4
+                        </button>
+                    </h2>
+                    <div id="flush-collapseFour" className="accordion-collapse collapse"
+                         aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">Placeholder content for this accordion, which is intended to
+                            demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion
+                            body.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingFive">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#flush-collapseFive" aria-expanded="false"
+                                aria-controls="flush-collapseFive">
+                            Accordion Item #5
+                        </button>
+                    </h2>
+                    <div id="flush-collapseFive" className="accordion-collapse collapse"
+                         aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">Placeholder content for this accordion, which is intended to
+                            demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion
+                            body.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </section>
 
         <section id="contact">
             <h4>Get In Touch</h4>
-            <p>Have questions or need assistance? Reach out to us anytime through our contact form, email, or phone. We're here to help!</p>
+            <p>Have questions or need assistance? Reach out to us anytime through our contact form, email, or phone.
+                We're here to help!</p>
         </section>
 
         <footer>
