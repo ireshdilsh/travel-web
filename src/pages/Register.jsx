9629f5834967abe734514888fc85b665130f3046
ireshdilsh <ireshdilshan46@gmail.com>
2025-02-16 18:03:42 +0530

    romove unused imports

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,6 @@ import "../styles/Register.css"
 import logo from "../assets/Literary.png"
 import {Link, useNavigate} from "react-router-dom";
 import Swal from "sweetalert2";
-import axios from 'axios';
 
 export default function Register() {
 
