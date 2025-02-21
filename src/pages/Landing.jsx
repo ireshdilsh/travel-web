7e397bc7f700c254c4f3493275e50d3f86d5bda4
ireshdilsh <ireshdilshan46@gmail.com>
2025-02-21 11:20:41 +0530

    Enhance Landing component with new content and styling improvements

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,10 +6,13 @@ export default function Landing() {
     <div>
       <section id='home'>
         <div className="left-side">
-
+            <p className='title'>Unlock Your Potential <span>Learn <br />Anytime,</span> Anywhere</p>
+            <p>Join our online learning platform to access diverse courses, expert instructors, and flexible schedules. <br />Start your journey today!</p>
         </div>
         <div className="right-side">
-          
+            <div className="border">
+
+            </div>
         </div>
       </section>
     </div>
