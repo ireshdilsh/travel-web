fa783aa71b25b51d3517503537dc33562cdc08ee
ireshdilsh <ireshdilshan46@gmail.com>
2025-02-21 11:01:49 +0530

    Add structure to Landing component and introduce global styles

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,7 +4,14 @@ import "../styles/Landing.css"
 export default function Landing() {
   return (
     <div>
-      
+      <section id='home'>
+        <div className="left-side">
+
+        </div>
+        <div className="right-side">
+          
+        </div>
+      </section>
     </div>
   )
 }
