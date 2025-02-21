9544d878eece83c7bd2ed2e748acf4c5b854da49
ireshdilsh <ireshdilshan46@gmail.com>
2025-02-21 23:24:38 +0530

    Add Learning GIF and enhance Landing component layout and styles

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../styles/Landing.css"
 import heroImage from "../assets/hero-image.png"
+import gif1 from "../assets/Learning.gif"
 
 export default function Landing() {
   return (
@@ -8,16 +9,17 @@ export default function Landing() {
 
       <nav>
         <div className="logo-and-name">
-
+            <h3>LearnSphere</h3>
         </div>
         <div className="links">
-          <a href="#home"></a>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
+          <a href="#home">Home</a>
+          <a href="#courses">Courses</a>
+          <a href="">About Us</a>
+          <a href="">Instructors</a>
+          <a href="">Blogs</a>
+          <a href="">Resources </a>
+          <a href="">FAQS</a>
+          <a href="">Contact Us</a>
         </div>
         <div className="sign-btn">
             <a href="">Sign In</a>
@@ -39,6 +41,19 @@ export default function Landing() {
             <div className="circle"></div>
             <div className="smaill-circle-1"></div>
             <div className="smaill-circle-2"></div>
+        </div>
+      </section>
+
+      <section id='courses'>
+        <div className="left-side">
+           <img src={gif1} alt="" />
+        </div>
+        <div className="right-side">
+        <h1>Explore Our Courses</h1>
+        <p>Browse through our extensive catalog of courses across various subjects. Whether you're looking to enhance your skills, pursue a new hobby, or advance your career, we have the right course for you. Join our community of learners and start learning today!</p>
+         <div className="explore-btn">
+         <a href="">Explore Courses</a>
+         </div>
         </div>
       </section>
     </div>
