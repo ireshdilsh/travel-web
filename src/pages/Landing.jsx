c06a4995534472463ec7b085ef48dae5cdd16a5e
ireshdilsh <ireshdilshan46@gmail.com>
2025-02-23 22:19:34 +0530

    Remove unused assets and clean up Landing component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "../styles/Landing.css"
-import img1 from "../assets/hero.jpg"
 
 export default function Landing() {
   return (
     <div>
+
+        
+
     </div>
   )
 }
