92097fb36c0446d2559750f579af17db980ec6d5
ireshdilsh <ireshdilshan46@gmail.com>
2025-03-04 22:37:17 +0530

    Remove unused Login component markup for cleaner code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,47 +1,12 @@
 import React from 'react'
 import "../styles/Login.css"
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Login() {
 
     return (
         <div>
-            <div className="login-card">
-                <div className="card-body">
-                    <div className="logo">
-                        <img src={logo} alt="" />
-                    </div>
-                    <div className="title">
-                        <h4>Sign your Account</h4>
-                    </div>
-                    <div className="discription">
-                        <p>Enter your credintails for sign in your account!</p>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
-                    </div>
-                    <div className="signin-btn">
-                        <button className="btn btn-primary" type="submit">Sign In</button>
-                    </div>
-                    <div className="line-and-or">
-                        <hr />
-                        <label>Or</label>
-                    </div>
-                    <div className="continue-google">
-                        <button className="btn btn-light">Continue with Google <i class="bi bi-google"></i></button>
-                    </div>
-                    <div className="link-and-text">
-                        <p>If you haven't an account ?</p>
-                        <Link to= "/auth/create_account">Create Account</Link>
-                    </div>
-                </div>
-            </div>
+
         </div>
     )
 }
