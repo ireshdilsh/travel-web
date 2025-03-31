8f5787480c8d34d87b7848ab972f89ee40b3c081
ireshdilsh <ireshdilshan46@gmail.com>
2025-03-31 20:37:00 +0530

    Add button to Register page for improved user interaction

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 export default function Register() {
 
     return (
-        <div>This is Register Page</div>
+        <div>This is Register Page
+            <button>Thsi is Register Btn</button>
+        </div>
     )
 }
