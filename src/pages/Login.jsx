d6615e6bcb8c86bd439e5eb9f7cd3ff2670a7c5c
ireshdilsh <ireshdilshan46@gmail.com>
2025-04-01 22:53:56 +0530

    update full code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 export default function Login() {
+
+  const name = () => {
+    alert("This is Alert")
+  }
+
   return (
     <div>
       <h1>This is Login Page</h1>
-      <button>Sign in Btn</button>
+      <button>Log in Btn</button>
+
     </div>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Register() {
 
     return (
-        <div>This is Register Page
-            <button>Thsi is Register Btn</button>
+        <div>
+            
         </div>
     )
 }
