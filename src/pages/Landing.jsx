643826c743368d547f3b2128cfa2f465ce40929b
ireshdilsh <ireshdilshan46@gmail.com>
2025-05-25 22:56:45 +0530

    fix: adjust video autoplay attribute and improve navigation layout in Landing component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,11 +5,22 @@ export default function Landing() {
   return (
     <div>
        <video src={heroVideo} 
+       
         autoPlay = "true"
         loop
         muted
         playsInline>
         </video>
+
+        <nav>
+          <div className="links">
+            <a href="">Home</a>
+            <a href="">Places</a>
+            <a href="">Images</a>
+            <a href=""></a>
+            <a href=""></a>
+          </div>
+        </nav>
     </div>
   )
 }
